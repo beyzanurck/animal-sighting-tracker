a653476bc046b6b7ecd5f001fb9555efeee309bb
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-09-20 05:44:14 -0500

    gets all data from 3 tables

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,58 @@
 import './App.css'
+import { useEffect } from 'react';
 
 
 function App() {
+
+  async function getSpecies() {
+    try {
+      const response = await fetch('http://localhost:3000/species');
+
+      if(!response.ok) {
+        throw new Error('response was not ok');
+      }
+
+      const allSpecies = await response.json();
+      console.log(allSpecies)
+      
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async function getIndividuals() {
+    try {
+      const response = await fetch('http://localhost:3000/individuals');
+
+      if(!response.ok) {
+        throw new Error('response was not ok');
+      }
+
+      const allIndividuals = await response.json();
+      console.log(allIndividuals)
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async function getSightings() {
+    try {
+      const response = await fetch('http://localhost:3000/sightings');
+
+      if(!response.ok) {
+        throw new Error('response was not ok');
+      }
+
+      const allSightings = await response.json();
+      console.log(allSightings)
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  useEffect(() => {getSpecies(), getIndividuals(), getSightings()}, []);
 
   return (
     <div className="App">
