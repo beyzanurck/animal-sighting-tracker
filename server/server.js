656970dc20dbff8f82543fe3ee43d34be6d9dd90
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-09-20 04:30:05 -0500

    implements 3 requests for the individuals table

--- server/server.js
+++ server/server.js
@@ -61,11 +61,71 @@ app.put('/species/:id', async (req, res) => {
 app.delete('/species/:id', async (req, res) => {
     try {
         const {id} = req.params;
-        console.log("id is here " + id)
         const deleteSpecies = await db.query("DELETE FROM species WHERE id = $1", [id]
         );
 
-        res.json("Event was deleted!");
+        res.json("The species was deleted!");
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(400).json({error});
+    }
+})
+
+//INDIVIDUALS
+app.get('/individuals', async (req, res) => {
+    try {
+        const {rows : individuals} = await db.query('SELECT * FROM individuals');
+        res.send(individuals);
+    } catch (error) {
+        console.error("Error fetching species:", error.message);
+        res.status(500).send({ error: "Failed to fetch species." });
+    }
+});
+
+app.post('/individuals', async (req, res) => {
+    try {
+        
+        const {nickname, scientist_tracking, species_id, created_at } = req.body;
+
+        const newIndividual =  await db.query("INSERT INTO individuals (nickname, scientist_tracking, species_id, created_at) VALUES ($1, $2, $3, $4) RETURNING *", [nickname, scientist_tracking, species_id, created_at]);
+
+        res.json(newIndividual.rows[0])
+        
+    } catch (error) {
+        console.error(error.message)
+        res.status(400).json({error});
+
+    }
+})
+
+app.put('/individuals/:id', async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        const {nickname, scientist_tracking, species_id, created_at } = req.body;
+
+        const editedIndividual = await db.query("UPDATE individuals SET nickname = $1, scientist_tracking = $2, species_id = $3, created_at = $4 WHERE id = $5 RETURNING *", [nickname, scientist_tracking, species_id, created_at, id]);
+
+        res.json(editedIndividual.rows[0])
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(400).json({error});
+    }
+
+})
+
+app.delete('/individuals/:id', async (req, res) => {
+    try {
+
+        const {id} = req.params;
+        const deleteIndividual = await db.query("DELETE FROM individuals WHERE id = $1", [id]
+        );
+
+        res.json("The individual was deleted!");
 
     } catch (error) {
         console.error(error.message)
